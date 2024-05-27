76f293721b027ab80cc7b4875558ddee23974ba6
Your Name <you@example.com>
2024-05-27 17:16:55 +0530

    commited

---
+++ src/components/News.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+
+
+
+export default class News extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>hy i am news</h1>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+
+        </div>
+      
+    )
+  }
+}
---
+++ src/components/Newsitem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+     <h1>hy this is siddhant</h1>
+      </div>
+    );
+  }
+}
